67e5c051cab5366cdd3ed33b71905c92f3d5e2e2
Kanishk Verma <kanishkverma@Kanishks-MacBook-Pro.local>
2017-08-06 12:24:35 +0530

    Initial Commit

---
+++ ascii convertor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ascii convertor
+//
+//  Created by Kanishk Verma on 06/08/17.
+//  Copyright © 2017 Kanishk Verma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
